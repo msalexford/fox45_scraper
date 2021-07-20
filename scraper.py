164dc5f457a574f05a2b19e8cd90745bd0f66caa
Alex Ford <52706746+msalexford@users.noreply.github.com>
2021-07-20 12:19:40 -0400

    Add files via upload
    Another attempt at building a scraper that works with the Chrome Web Driver. This attempt uses ChromeDriveManager as suggested here: https://www.edureka.co/community/3423/exceptions-webdriverexception-chromedriver-executable

--- scraper.py
+++ scraper.py
@@ -1,20 +1,25 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[7]:
-
+# In this attempt, I am trying to get the Chrome Driver to install in GH Actions
+# using the ChromeDriverManager
 
 import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 import re
 
-# Initiate a headless driver.
+# Adding headless options to avoid opening the webpage.
 
 headless_options = Options()
 headless_options.add_argument('--headless')
 
-driver = webdriver.Chrome('./chromedriver', options=headless_options)
+# Installing the driver with the headless options.
+
+driver = webdriver.Chrome(ChromeDriverManager().install(), options=headless_options)
+
+# Accessing the Fox website.
 
 driver.get('https://foxbaltimore.com/')
 driver.set_page_load_timeout(30)
@@ -29,6 +34,8 @@ for link in homepage:
 
 headlines = link.text.splitlines()
 df = pd.DataFrame(headlines)
+
+# print(df)
 
 # Export as CSV.
 
