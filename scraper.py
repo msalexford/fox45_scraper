5ca5aa116f86c3d9c8d429998642fd959bfb4714
Alex Ford <52706746+msalexford@users.noreply.github.com>
2021-08-28 11:29:23 -0500

    Update scraper.py
    Fixing date issue.

--- scraper.py
+++ scraper.py
@@ -19,12 +19,20 @@ scraped_date = driver.find_element_by_xpath('/html/head/meta[30]').get_attribute
 homepage = driver.find_elements_by_xpath("//*[contains(@id, 'js-TopStories-Container')]")
 
 for link in homepage:
-    print(link.text, scraped_date)
+    try:
+        print(link.text)
+    except:
+        print('no headline')
     
 headlines = link.text.splitlines()
 
-df = pd.DataFrame(headlines)
+scraped_fox_headlines = {
+    'headline': headlines,
+    'date': scraped_date
+}
 
-pd.read_csv('fox45_headlines.csv').append(df).drop_duplicates().to_csv('fox45_headlines.csv', sep=',', header=None, index=None)
+df = pd.DataFrame(scraped_fox_headlines)
 
-# df.to_csv('fox45_headlines.csv', sep=',', header=None, index=None)
+df.columns = ['headline', 'date']
+
+pd.read_csv('fox45_headlines.csv').append(df).drop_duplicates().to_csv('fox45_headlines.csv')
