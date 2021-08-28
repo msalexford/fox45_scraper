f400c72e861569aba7168eed722ab1466db31611
Alex Ford <52706746+msalexford@users.noreply.github.com>
2021-08-28 11:42:45 -0500

    Update scraper.py
    More edits to try to fix slow action time.

--- scraper.py
+++ scraper.py
@@ -12,17 +12,12 @@ driver = webdriver.Chrome(ChromeDriverManager().install(), options=headless_opti
 driver.get('https://foxbaltimore.com/')
 driver.set_page_load_timeout(30)
 
-fox_results = []
-
 scraped_date = driver.find_element_by_xpath('/html/head/meta[30]').get_attribute('content')
 
 homepage = driver.find_elements_by_xpath("//*[contains(@id, 'js-TopStories-Container')]")
 
 for link in homepage:
-    try:
-        print(link.text)
-    except:
-        print('no headline')
+    print(link.text)
     
 headlines = link.text.splitlines()
 
@@ -32,7 +27,5 @@ scraped_fox_headlines = {
 }
 
 df = pd.DataFrame(scraped_fox_headlines)
-
-df.columns = ['headline', 'date']
 
 pd.read_csv('fox45_headlines.csv').append(df).drop_duplicates().to_csv('fox45_headlines.csv')
